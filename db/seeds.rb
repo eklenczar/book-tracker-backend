ca3eaa986163327db48081dbdbfdd4b8d62c1d45
Elizabeth Klenczar <eklenczar@gmail.com>
2023-05-21 06:21:57 -0400

    Add User, Book, and Review resources. Add Faker gem. Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.destroy_all
+Book.destroy_all
+Review.destroy_all
+
+puts "🌱 Seeding..."
+
+u1 = User.create(name: "Bob", email: "bob@ross.com", password: "bob")
+u2 = User.create(name: "Carl", email: "carl@sagan.com", password: "carl")
+u3 = User.create(name: "Bill", email: "bill@nye.com", password: "bill")
+
+users = User.all.map{|u| u.id}
+
+15.times do
+    Book.create(
+        title: Faker::Book.title,
+        author: Faker::Book.author,
+        genre: Faker::Book.genre
+    )
+end
+
+books = Book.all.map{|b| b.id}
+
+20.times do
+    Review.create(
+        title: Faker::Lorem.sentence(word_count: 3),
+        text: Faker::Lorem.paragraph,
+        user_id: users.sample,
+        book_id: books.sample
+    )
+end
+
+puts "✅ Done seeding!"
+

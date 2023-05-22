8122a9e1f744fa00f0d7d9ad2d2ffac4f4150f0a
Elizabeth Klenczar <eklenczar@gmail.com>
2023-05-22 19:59:24 -0400

    Add CRUD to books_controller and error handling.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,46 @@
 class BooksController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+    def index
+        books = Books.all
+        render_json: books, status: :ok
+    end
+    
+    # GET /books/:id
+    def show
+        book = find_book
+        render json: book
+    end
+
+    def create
+        book = Book.create(book_params)
+        render json: book, status: :created
+    end
+
+    # PATCH /books/:id
+    def update
+        book = find_book
+        book.update(book_params)
+        render json: book
+    end
+
+      def destroy
+        book = find_book
+        book.destroy
+        head :no_content
+      end
+
+    private
+
+    def book_params
+        params.permit(:title, :author, :genre)
+    end
+
+    def render_not_found_response
+        render json: { error: "Book not found" }, status: :not_found
+    end
+
+    def find_book
+        Book.find(id: params[:id])
+    end
 end
